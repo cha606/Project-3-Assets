836a893af50eb4ccc0155935b411e5630377e4f6
Charles Richards <richardsc@bxscience.edu>
2019-12-12 00:00:19 -0500

    Parry System just need to be tested after the other components are done (Im waiting on the character scripts) Folders are made and The mo assetwas added

---
+++ Project 3 Andy and the Maze/Assets/Scripts/Manager/ParrySystem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParrySystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject Player;
+    GameObject GUI;
+    char ParryButton;
+    void Start()
+    {
+        Player= GameObject.FindWithTag("Player");
+        GUI.setActive=false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Parry(GameObject Attacker, GameObject Defender)
+    {
+        //Player.CanBeAttacked = false;
+        bool Success = false;
+
+        GUI.setActive=true;
+        yield return new WaitForSeconds(5);
+        if (Input.GetKey(KeyCode.ParryButton)){Success = true;}
+        if (Success){Destroy(Attacker);}  //We could have an animation
+        //else(Player.Health -=1);
+
+    }
+    
+}
