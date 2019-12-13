0bbc3bd5b861e3a9515005442eefb33c8e1c8147
cha606 <33331069+cha606@users.noreply.github.com>
2019-12-13 10:59:52 -0500

    Started player movement

---
+++ Project 3 Andy and the Maze/Assets/Scripts/Character/Player Script.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    float health;
+    float speed;
+    GameObject Body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Body = GetComponent<Rigidbody>();
+        health = 100f;
+        speed = 10f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W)) {
+            Body.transform.position;
+        }
+    }
+}
